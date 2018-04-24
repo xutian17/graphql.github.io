1ef2cccf8dac0415446156411c789880894fe11c
Xu Tian <31019145+xutian17@users.noreply.github.com>
2018-04-23 21:25:29 -0700

    Update index.html.js

--- site/users/index.html.js
+++ site/users/index.html.js
@@ -601,6 +601,11 @@ var logos = [
     img: 'universe.png',
     link: 'https://www.universe.com'
   },
+  {
+    name: 'Houzz',
+    img: 'houzz.png',
+    link: 'https://www.houzz.com'
+  }
   // Adding your logo?
   // Add it to the /users/logos/ directory and then append an entry above this comment.
   //
